af85ac47cc17f7ba9b71f93072341f09075707f8
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-21 19:57:59 +0300

    Create Hero.jsx

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+
+
+const Hero = () => {
+    return (
+        <section>
+        <h1>Hi&#128075;</h1>
+        <p>Welcome to MacPaw Bootcamp 2023</p>
+        <p>Lets start using The Cat API</p>
+
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        </section>
+        
+    )
+}
+
+export default Hero;
