ac27fccdb53344b007fabd76aad01bee5815fd92
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-21 19:57:43 +0300

    Create HeaderStyled.js

---
+++ src/components/Header/HeaderStyled.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const HeaderStyled = styled.header`
+  padding-left: 147px;
+  margin-bottom: 50px;
+`;
+
+export const LogoLink = styled.a`
+  text-decoration: none;
+  display: inline-block;
+  padding-top: 30px;
+  padding-bottom: 30px;
+`;
