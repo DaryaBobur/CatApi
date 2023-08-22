5634d275da7b5e85e3b18099b88e41640e22af36
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-22 20:27:15 +0300

    Create HeroStyled.js

---
+++ src/components/Hero/HeroStyled.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const HeroTitle = styled.h1`
+  font-size: 44px;
+  font-weight: 500;
+  line-height: 1.32;
+
+  margin-bottom: 10px;
+`;
+export const ContainerStyled = styled.div`
+  padding-left: 147px;
+`;
+
+export const Section = styled.section`
+  padding-top: 50px;
+`;
+
+export const Description = styled.p`
+  color: #8c8c8c;
+  font-size: 20px;
+  margin-bottom: 60px;
+`;
+
+export const Text = styled.p`
+  font-size: 20px;
+  font-weight: 500;
+  margin-bottom: 20px;
+`;
+
+export const NavList = styled.ul`
+  display: flex;
+  gap: 16px;
+  list-style: none;
+`;
+
+
+export const VotingCardImg = styled.div`
+  padding: 37px 19px;
+
+  border-radius: 20px;
+
+  border: 4px solid rgba(255, 255, 255, 0.6);
+  background: #b4b7ff;
+`;
+
+export const BreedsCardImg = styled.div`
+  padding: 17px 11px 18px 10px;
+
+  border-radius: 20px;
+  border: 4px solid rgba(255, 255, 255, 0.6);
+  background: #97eab9;
+`;
+
+export const GalleryCardImg = styled.div`
+  padding: 4px 13px;
+
+  border-radius: 20px;
+  border: 4px solid rgba(255, 255, 255, 0.6);
+  background: #ffd280;
+`;
+
+export const Category = styled.h2`
+color: #FF868E;
+text-align: center;
+font-size: 12px;
+font-weight: 500;
+line-height: 1.33; 
+letter-spacing: 2px;
+
+margin-top: 10px;
+padding: 10px 0px;
+border-radius: 10px;
+background-color: #FFF;
+`
