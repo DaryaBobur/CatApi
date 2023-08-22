9b990da7f96fdf4ef8b4a39162fd77d990ebfb58
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-22 20:26:33 +0300

    Create App.jsx

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+import Hero from "../Hero/Hero";
+import {Container} from "../App/AppStyled" 
+
+export const App = () => {
+  return (
+    <Container>
+      <Header/>
+      <Hero/>
+    </Container>
+  );
+};
