1a5e2e4ec2f2b394dcd7a2135d1b9385a2a1912c
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-22 20:27:12 +0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,50 @@
-
+import vote from '../../images/hero-icons/vote-table.svg';
+import cat from '../../images/hero-icons/pet-breeds.svg';
+import gallery from '../../images/hero-icons/images-search.svg';
+import {
+  HeroTitle,
+  ContainerStyled,
+  Section,
+  Description,
+  Text,
+  NavList,
+  VotingCardImg,
+  BreedsCardImg,
+  GalleryCardImg,
+  Category
+} from './HeroStyled';
 
 const Hero = () => {
-    return (
-        <section>
-        <h1>Hi&#128075;</h1>
-        <p>Welcome to MacPaw Bootcamp 2023</p>
-        <p>Lets start using The Cat API</p>
+  return (
+    <Section>
+      <ContainerStyled>
+        <HeroTitle>Hi&#128075;</HeroTitle>
+        <Description>Welcome to MacPaw Bootcamp 2023</Description>
+        <Text>Lets start using The Cat API</Text>
 
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        </section>
-        
-    )
-}
+        <NavList>
+          <li>
+            <VotingCardImg>
+              <img src={vote} alt="vote" />
+            </VotingCardImg>
+            <Category>VOTING</Category>
+          </li>
+          <li>
+            <BreedsCardImg>
+              <img src={cat} alt="cat breeds" />
+            </BreedsCardImg>
+            <Category>BREEDS</Category>
+          </li>
+          <li>
+            <GalleryCardImg>
+              <img src={gallery} alt="gallery" />
+            </GalleryCardImg>
+            <Category>GALLERY</Category>
+          </li>
+        </NavList>
+      </ContainerStyled>
+    </Section>
+  );
+};
 
 export default Hero;
