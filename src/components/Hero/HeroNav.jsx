268b62c232f0ff9d97ebcbca27c119f5245fd4b2
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-25 16:31:02 +0300

    Create HeroNav.jsx

---
+++ src/components/Hero/HeroNav.jsx
@@ -0,0 +1,56 @@
+import vote from '../../images/hero-icons/vote-table.svg';
+import cat from '../../images/hero-icons/pet-breeds.svg';
+import gallery from '../../images/hero-icons/images-search.svg';
+import {
+  HeroTitle,
+  ContainerStyled,
+  Section,
+  Description,
+  Text,
+  NavList,
+  VotingCardImg,
+  BreedsCardImg,
+  GalleryCardImg,
+  Category,
+} from './HeroStyled';
+
+const HeroNav = () => {
+  return (
+    <Section>
+      <ContainerStyled>
+        <HeroTitle>Hi&#128075;</HeroTitle>
+        <Description>Welcome to MacPaw Bootcamp 2023</Description>
+        <Text>Lets start using The Cat API</Text>
+
+        <NavList>
+          <li>
+            <a href="">
+              <VotingCardImg>
+                <img src={vote} alt="vote" />
+              </VotingCardImg>
+              <Category>VOTING</Category>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <BreedsCardImg>
+                <img src={cat} alt="cat breeds" />
+              </BreedsCardImg>
+              <Category>BREEDS</Category>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <GalleryCardImg>
+                <img src={gallery} alt="gallery" />
+              </GalleryCardImg>
+              <Category>GALLERY</Category>
+            </a>
+          </li>
+        </NavList>
+      </ContainerStyled>
+    </Section>
+  );
+};
+
+export default HeroNav;
