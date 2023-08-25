80da0333e986d11825f84c3440364a5313248426
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-25 16:31:10 +0300

    Create HomePageStyled.js

---
+++ src/pages/Home/HomePageStyled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.div`
+position: fixed;
+width: 50%;
+`
+
+export const Section = styled.section`
+width: 1440px; 
+margin-left: auto;
+margin-right: auto;
+`
