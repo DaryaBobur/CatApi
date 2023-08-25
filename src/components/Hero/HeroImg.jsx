b7b8f87828fb9fb8fe1cb6d2294414a2f603b017
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-25 16:30:47 +0300

    Create HeroImg.jsx

---
+++ src/components/Hero/HeroImg.jsx
@@ -0,0 +1,12 @@
+import { ContainerImg, ContainerBackground } from './HeroStyled';
+const HeroImg = () => {
+  return (
+    <>
+      <ContainerBackground></ContainerBackground>
+
+      <ContainerImg></ContainerImg>
+    </>
+  );
+};
+
+export default HeroImg;
