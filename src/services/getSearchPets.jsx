ef5d99f88d189aaeff5fcad8e679bac98dd531b9
Daria <97470059+DaryaBobur@users.noreply.github.com>
2023-08-26 15:10:58 +0300

    Create getSearchPets.jsx

---
+++ src/services/getSearchPets.jsx
@@ -0,0 +1,6 @@
+// 'https://api.thecatapi.com/v1/images/search?api_key=live_JHS4rTvyVxTte29MYXHlEWPAMtvqyAO1fjYS0t5xgNTupQdXfJ9qBvGal3kkLZbc'
+
+import axios from "axios";
+
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const KEY = 'api_key=live_JHS4rTvyVxTte29MYXHlEWPAMtvqyAO1fjYS0t5xgNTupQdXfJ9qBvGal3kkLZbc';
